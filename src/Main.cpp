0c44aa8f89eb7c08b6ed536cef6702e0e84ae7dd
Garoze <marcelogaroze@hotmail.com>
2022-05-13 23:04:06 -0300

    feat: Some tests for the new instructions

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,9 +6,14 @@
 int main()
 {
     CPU cpu;
+    cpu.setDebug(true);
 
     const std::vector<std::uint8_t> code = {
-        (int)Opcodes::NOP,
+        (int)Opcodes::LDI, 0x01, 0xCD, 0xAB,
+        (int)Opcodes::STR, 0xEF, 0xFF, 0x01,
+        (int)Opcodes::STA, 0xFF, 0xFF, 0xEF, 0xFF,
+        (int)Opcodes::LDA, 0x02, 0xEF, 0xFF,
+        (int)Opcodes::OUT, 0x02,
         (int)Opcodes::HLT,
     };
     cpu.loadToMemory(code);
@@ -16,6 +21,8 @@ int main()
 
     printf("\nMain Memory:\n");
     cpu.viewMemory(0x0000, 2);
+    printf("\nStack Memory:\n");
+    cpu.viewMemory(0xFFEF, 2);
 
     return EXIT_SUCCESS;
 }
